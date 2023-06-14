d9e1d0f5266973cc8f7e590ca0e7e059f4563aec
GowthamiKarnati <113590933+GowthamiKarnati@users.noreply.github.com>
2023-06-14 16:51:45 +0530

    c0

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,7 +9,7 @@ const LoginPage = () => {
     const {setUserInfo} = useContext(UserContext);
     async function login(ev) {
       ev.preventDefault();
-      const response = await fetch('http://localhost:4000/login', {
+      const response = await fetch('https://thoughtful-fox-leotard.cyclic.app/login', {
         method: 'POST',
         body: JSON.stringify({username, password}),
         headers: {'Content-Type':'application/json'},
